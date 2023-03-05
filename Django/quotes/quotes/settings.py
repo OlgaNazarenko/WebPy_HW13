4ac53144042c8df02d7de955454da7379d25cc23
olganazarenko <113229061+olganazarenko@users.noreply.github.com>
2023-03-05 20:46:53 +0200

    updated dotenv

--- Django/quotes/quotes/settings.py
+++ Django/quotes/quotes/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 print(f'{load_dotenv(find_dotenv())=}')
